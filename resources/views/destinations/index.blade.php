f1ce33d7fdedb277c6e7d318fa23f54d88b3c516
enjirahma <enjirahma2@gmail.com>
2026-01-10 19:21:43 +0800

    mengubah index

--- resources/views/destinations/index.blade.php
+++ resources/views/destinations/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="d-flex justify-content-between mb-3">
+<div class="d-flex justify-content-between align-items-center mb-4">
     <h3 class="fw-bold">Data Destinasi</h3>
     <a href="{{ route('destinations.create') }}" class="btn btn-primary">
         + Tambah Destinasi
@@ -14,8 +14,9 @@
     </div>
 @endif
 
-<table class="table table-bordered table-striped">
-    <thead>
+@if($destinations->count())
+<table class="table table-bordered table-striped align-middle">
+    <thead class="table-dark">
         <tr>
             <th>Nama</th>
             <th>Lokasi</th>
@@ -29,21 +30,27 @@
             <td>{{ $item->name }}</td>
             <td>{{ $item->location }}</td>
             <td>
-                <img src="{{ asset('images/'.$item->image) }}" width="100">
+                @if($item->image)
+                    <img src="{{ asset('storage/'.$item->image) }}"
+                         width="100"
+                         class="rounded shadow-sm">
+                @else
+                    <span class="text-muted">Tidak ada gambar</span>
+                @endif
             </td>
             <td>
-                <a href="{{ route('destinations.show',$item->id) }}"
-                   class="btn btn-info btn-sm">View</a>
+                <a href="{{ route('destinations.show', $item->id) }}"
+                   class="btn btn-info btn-sm mb-1">View</a>
 
-                <a href="{{ route('destinations.edit',$item->id) }}"
-                   class="btn btn-warning btn-sm">Edit</a>
+                <a href="{{ route('destinations.edit', $item->id) }}"
+                   class="btn btn-warning btn-sm mb-1">Edit</a>
 
-                <form action="{{ route('destinations.destroy',$item->id) }}"
+                <form action="{{ route('destinations.destroy', $item->id) }}"
                       method="POST" class="d-inline">
                     @csrf
                     @method('DELETE')
                     <button class="btn btn-danger btn-sm"
-                            onclick="return confirm('Hapus data?')">
+                            onclick="return confirm('Yakin hapus data ini?')">
                         Delete
                     </button>
                 </form>
@@ -52,4 +59,9 @@
         @endforeach
     </tbody>
 </table>
+@else
+    <div class="alert alert-info">
+        Belum ada data destinasi.
+    </div>
+@endif
 @endsection
